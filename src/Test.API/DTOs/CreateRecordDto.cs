9d734f5827cc9e383eec9ccff7b318b63b9286c3
tiurinatamila <s30844@pjwstk.edu.pl>
2025-06-13 11:48:11 +0200

    final

--- src/Test.API/DTOs/CreateRecordDto.cs
+++ src/Test.API/DTOs/CreateRecordDto.cs
@@ -2,12 +2,14 @@ namespace Test.API.DTOs;
 
 public class CreateRecordDto
 {
+    
     public int LanguageId { get; set; }
     public int StudentId { get; set; }
     public int? TaskId { get; set; }
 
     public CreateTaskDto? Task { get; set; }
-
+    
+    
     public long ExecutionTime { get; set; }
     public DateTime Created { get; set; }
 }
--- src/Test.API/DTOs/CreateTaskDto.cs
+++ src/Test.API/DTOs/CreateTaskDto.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Test.API.DTOs;
 
 public class CreateTaskDto
 {
+    [Length(1,100)]
     public string Name { get; set; }
+    
+    [Length(1,2000)]
     public string Description { get; set; }
 }
